c32d829d350e600187764a3ea3402bbc6883f8a2
Senevirathna12 <anishantha702@gmail.com>
2025-09-02 22:22:38 +0530

    Learned while loop

---
+++ whileloop.py
@@ -0,0 +1,7 @@
+name = input(f"Enter your name : ")
+
+while name  == "":
+    print(f"Please enter your name again...")
+    name = input(f"Enter your name : ")
+else :
+    print(f"This is your name {name}")
