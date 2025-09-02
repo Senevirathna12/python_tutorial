847fbd13eacb208eaf2cc24cadc3930845d08726
Senevirathna12 <anishantha702@gmail.com>
2025-09-02 17:53:49 +0530

    conditional statement

---
+++ conditional.py
@@ -0,0 +1,15 @@
+# x = 1
+# print(x == 1)
+# print(x != 1)
+# print(x > 1)
+# print(x < 1)
+# print(x >= 1)
+# print(x <= 1)
+
+name = "amith"
+age = 26
+
+if name == "amith" and age == 26:
+    print('Hi Amith..!')
+else :
+    print('Go awai...')
