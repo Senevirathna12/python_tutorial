738fee9eb437e900a5d8d8c6a8e6b6a6fc648f58
Senevirathna12 <anishantha702@gmail.com>
2025-09-02 14:40:31 +0530

    Get user first, middle , last names and age as a user input and show the full name with age

---
+++ Problem001.py
@@ -0,0 +1,8 @@
+First_Name = input("Enter your frist name : ")
+Middle_Name = input("Enter your Middle name : ")
+Last_Name = input("Enter your Last name : ")
+Age = input("Enter your age name : ")
+
+Full_Name = First_Name + " " + Middle_Name + " " + Last_Name
+
+print('Your full name is ' + Full_Name + " And your age is " + str(Age) )
