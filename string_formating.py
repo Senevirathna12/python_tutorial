fcf878149fad750dbf401b0209f4bb092ba0daf3
Senevirathna12 <anishantha702@gmail.com>
2025-09-02 18:27:16 +0530

    learned some string formating techniques

---
+++ string_formating.py
@@ -0,0 +1,13 @@
+Amith_Data = ("Amith", 26, 5.4)  # --> Tuple -- immuatable - can't change
+# Amith_Data = ["Amith",26,5.4]  # --> List -- muatable - can change
+
+# So here can't use data as a list . But can be use as tuple.
+
+# name = 'amith'
+# age = 26
+# height = 5.4
+
+# print("Hi, My name is %s ,  My age is %d and also my height is %0.2f" %
+#       (name, age, height))
+
+print("Hi, My name is %s ,  My age is %d and also my height is %0.2f" % Amith_Data)
