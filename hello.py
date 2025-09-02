07c34cc735bffe3b2f6c2b969a548a0bb2c6bf60
Senevirathna12 <anishantha702@gmail.com>
2025-09-02 14:30:03 +0530

    variables and chnaging the data types

--- hello.py
+++ hello.py
@@ -1,3 +1,17 @@
-X = 2
-Y = 4
-print("Sum of the two values is ", X+Y)
+# pylint: disable=missing-module-docstring
+Name = "Amith"
+Age = 26
+
+print("My name is " + Name)
+print("My age is " + str(Age))
+
+X = "6"
+Y = "8"
+
+print("multiplication of X and Y =", eval(X) * eval(Y))
+
+# Variable types can be changed using the following methods
+# eval() --> Any data types convert to int
+# int()
+# str()
+# float()
