1cb96e388b2d78446875b3a76ae331d8e98cfeba
Senevirathna12 <anishantha702@gmail.com>
2025-09-02 18:01:44 +0530

    make sam simple problem using conditional statement and list

---
+++ Problem002.py
@@ -0,0 +1,10 @@
+menu = ["rice", "egg", "koththu"]
+
+food = input("can i get : ")
+
+if food in menu :
+    print("yes, "+food +" is available")
+else :
+    print("no, "+food +" isn't available")
+
+print("Thank You....!")
